a513bb33b2fa3ce935a56a9a4e8f7182b4809785
Uzzwal Dhali <d.uzzwal09@gmail.com>
2023-09-22 19:17:00 +0600

    Close button added

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,11 @@
   const showForm = () => {
     edit.value = true
   }
+
+  const closeForm = () => {
+    edit.value = false
+  }
+
   const newTask = ref('')
   const addItem = () => {
     list.value.push({
@@ -41,7 +46,7 @@
       </div>
     </div>
 
-    <div class="card" v-if="edit">
+    <div class="new_item_card" v-if="edit">
       <div class="body">
         <form @submit.prevent="addItem">
           <input
@@ -49,9 +54,9 @@
             v-model.trim="newTask"
             placeholder="Type and press enter"
           >
-          <!-- <button type="submit">Add</button> -->
         </form>
       </div>
+      <div class="cross" @click="closeForm">+</div>
     </div>
   </div>
 </template>
@@ -67,9 +72,9 @@
   gap: 2.5em;
   .card {
     background: white;
-    padding: 1.5em;
+    padding: 2.2em 1.8rem 3rem;
     border-radius: 0.75rem;
-    min-width: 350px;
+    min-width: 330px;
     display: flex;
     flex-direction: column;
     gap: 0.005em;
@@ -77,34 +82,16 @@
       font-size: 1.2em;
       display: flex;
       justify-content: space-between;
+      padding: 0.5rem 0 0.5rem;
       .title {
         font-size: 1em;
-        color: rgba(44, 53, 57, 0.75);
+        /* color: rgba(44, 53, 57, 0.75); */
+        color: rgb(15, 143, 152);
       }
 
     }
-
     .body {
-      form {
-        padding: 0.75em 0 0.5em;
-        display: flex;
-        gap: 0.25em;
-        justify-content: space-between;
-        input {
-          width: 100%;
-          outline: none;
-          border: none;
-          border-bottom: 1px solid rgba(44, 53, 57, 0.25);
-          padding: 0.25em 0em;
-        }
-        button {
-          background: rgb(15, 143, 152);
-          border-radius: 0.35em;
-          border: none;
-          color: white;
-          padding: 0.3em 0.75em;
-        }
-      }
+
       ul {
         margin-top: 0.5em;
         list-style: none;
@@ -137,13 +124,63 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        background: rgb(15, 143, 152);
+        background: rgba(15, 143, 152, 0.8);
         border: none;
         color: white;
         font-size: 1.7rem;
         left: 50%;
-        transform: translateX(-50%);
+        transform: translateX(-50%) translateY(50%);
       }
+      .add:hover {
+        background: rgba(15, 143, 152, 1);
+      }
+    }
+  }
+  .new_item_card {
+    background: white;
+    padding: 1.5rem 1.8rem;
+    border-radius: 0.75rem;
+    min-width: 330px;
+    .body {
+
+      form {
+        padding: 0.75em 0 0.5em;
+        display: flex;
+        gap: 0.25em;
+        justify-content: space-between;
+        input {
+          width: 100%;
+          outline: none;
+          border: none;
+          border-bottom: 1px solid rgba(44, 53, 57, 0.25);
+          padding: 0.25em 0em;
+        }
+        button {
+          background: rgb(15, 143, 152);
+          border-radius: 0.35em;
+          border: none;
+          color: white;
+          padding: 0.3em 0.75em;
+        }
+      }
+    }
+    .cross {
+      position: absolute;
+      width: 50px;
+      height: 50px;
+      background: rgba(226, 6, 112, 0.8);
+      border-radius: 50%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: white;
+      font-size: 1.7rem;
+      left: 50%;
+      transform: translateX(-50%) translateY(5%) rotate(45deg);
+    }
+    .cross:hover {
+      cursor: pointer;
+      background: rgb(226, 6, 112);
     }
   }
 }
