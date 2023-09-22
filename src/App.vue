afd36523efe33a1b631f12dcfefbff918477daec
Uzzwal Dhali <d.uzzwal09@gmail.com>
2023-09-22 15:44:07 +0600

    Redesigned

--- src/App.vue
+++ src/App.vue
@@ -19,23 +19,29 @@
   <div class="container">
     <div class="card">
       <div class="header">
-        <div class="title">Things to do</div>
-        <div class="actions">
-          <div @click="showForm" class="add">+</div>
-        </div>
+        <div class="title">{{ list.length }} Things to do</div>
       </div>
       <div class="body">
-        <form @submit.prevent="addItem" v-if="edit">
+
+        <ul>
+          <li v-for="item in list" :key="item.id" v-text="item" />
+        </ul>
+
+        <button @click="showForm" class="add">+</button>
+
+      </div>
+    </div>
+
+    <div class="card" v-if="edit">
+      <div class="body">
+        <form @submit.prevent="addItem">
           <input
             type="text"
             v-model.trim="newTask"
             placeholder="Type and press enter"
           >
-          <button type="submit">Add</button>
+          <!-- <button type="submit">Add</button> -->
         </form>
-        <ul>
-          <li v-for="item in list" :key="item.id" v-text="item" />
-      </ul>
       </div>
     </div>
   </div>
@@ -46,40 +52,40 @@
   background: #36454F;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 2.5em;
   .card {
+    background: white;
+    padding: 1.5em;
+    border-radius: 0.75rem;
     min-width: 350px;
     display: flex;
     flex-direction: column;
-    gap: 0.075em;
+    gap: 0.005em;
     .header {
-      background: white;
-      padding: 1.5em 1.5em 0.65em;
-      border-radius: 1em 1em 0 0;
       font-size: 1.2em;
       display: flex;
       justify-content: space-between;
-      .actions {
-        .add {
-          cursor: pointer;
-        }
+      .title {
+        font-size: 1em;
+        color: rgba(44, 53, 57, 0.75);
       }
+
     }
 
     .body {
-      background: white;
-      padding: 0.75em 1.5em 1.5em;
-      border-radius: 0 0 1em 1em;
       form {
-        padding: 1em 0 0.5em;
+        padding: 0.75em 0 0.5em;
         display: flex;
         gap: 0.25em;
         justify-content: space-between;
         input {
+          width: 100%;
           outline: none;
           border: none;
-          border-bottom: 1px solid rgba(44, 53, 57, 0.5);
+          border-bottom: 1px solid rgba(44, 53, 57, 0.25);
           padding: 0.25em 0em;
         }
         button {
@@ -96,6 +102,22 @@
         li {
           color: rgba(44, 53, 57, 0.75)
         }
+      }
+      .add {
+        position: absolute;
+        cursor: pointer;
+        border-radius: 50%;
+        width: 50px;
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: rgb(15, 143, 152);
+        border: none;
+        color: white;
+        font-size: 1.7rem;
+        left: 50%;
+        transform: translateX(-50%);
       }
     }
   }
